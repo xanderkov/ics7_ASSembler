367abf1fe1fe8c5161b2da97dfc6585c776ebaf1
Kovel Alexander <xanderkovel@gmail.com>
2022-03-31 15:43:07 +0300

    Added lab 06

---
+++ lab_06/calc.asm
@@ -0,0 +1,58 @@
+EXTRN NUMBER : WORD
+
+PUBLIC SHEX
+PUBLIC UBIN
+PUBLIC SIGN
+
+PUBLIC TOSHEX
+
+DATASEG SEGMENT PARA PUBLIC 'DATA'
+    MASK16 DW 15
+    MASK2 DW 1
+    SHEX DB 4 DUP('0'), '$'
+    UBIN DB 16 DUP('0'), '$'
+    SIGN DB ' '
+DATASEG ENDS
+
+CODESEG SEGMENT PARA PUBLIC 'CODE'
+    ASSUME CS:CODESEG, DS:DATASEG
+
+TOSHEX PROC NEAR
+    MOV AX, NUMBER
+    MOV BX, 3
+    GETHEX:
+        MOV DX, AX
+        AND DX, MASK16
+        CMP DL, 10
+        JB ISDIGIT
+        ADD DL, 7
+        ISDIGIT:
+        ADD DL, '0'
+        MOV SHEX[BX], DL
+        MOV CL, 4
+        SAR AX, CL
+        DEC BX
+        CMP BX, -1
+        JNE GETHEX
+    RET
+TOSHEX ENDP
+
+TOUBIN PROC NEAR
+
+    MOV AX, NUMBER
+    MOV BX, 15
+    GETBIN:
+        MOV DX, AX
+        AND DX, MASK2
+        ADD DL, '0'
+        MOV UBIN[BX], DL
+        MOV CL, 1
+        SAR AX, CL
+        DEC BX
+        CMP BX, -1
+        JNE GETBIN
+    RET
+TOUBIN ENDP
+
+CODESEG ENDS
+END
---
+++ lab_06/input.asm
@@ -0,0 +1,70 @@
+PUBLIC NUMBER
+PUBLIC INCMD
+PUBLIC INOCTSGN
+
+DATASEG SEGMENT PARA PUBLIC 'DATA'
+    NUMBER DW 0
+    INMSG DB 'Enter signed octal number: $'
+DATASEG ENDS
+
+CODESEG SEGMENT PARA PUBLIC 'CODE'
+    ASSUME CS:CODESEG, DS:DATASEG
+
+INCMD PROC NEAR
+    MOV AH, 1
+    INT 21H
+    SUB AL, '0'
+    MOV CL, 2
+    MUL CL
+    MOV SI, AX
+    RET
+INCMD ENDP
+
+ADDCIFR PROC NEAR
+    MOV CL, AL
+    MOV AX, 10
+    MUL BX
+    MOV BX, AX
+    MOV CH, 0
+    SUB CL, '0'
+    ADD BX, CX
+    RET
+ADDCIFR ENDP
+
+INOCTSGN PROC NEAR
+    MOV AH, 9
+    MOV DX, OFFSET INMSG
+    INT 21H
+
+    MOV BX, 0
+    MOV DX, 0
+    MOV AH, 1
+    INT 21H
+    CMP AL, 13
+    JE ENDINP
+    CMP AL, "-"
+        JE MAKESIGN
+    call ADDCIFR
+    MAKESIGN:
+        MOV DH, AL
+    
+    INSYMB:
+        MOV AH, 1
+        INT 21H
+        CMP AL, 13
+        JE ENDINP
+        call ADDCIFR
+        JMP INSYMB
+
+    ENDINP:
+    CMP DH, "-"
+    JNE NOTURNTONEG
+    NEG BX
+    NOTURNTONEG:
+    MOV NUMBER, BX
+
+    RET
+INOCTSGN ENDP
+
+CODESEG ENDS
+END
